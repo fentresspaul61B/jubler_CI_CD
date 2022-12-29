bc2f78a5a87e4c1517833eb706c79f7ba641ca6d
fentresspaul61B <fentresspaul@berkeley.edu>
2022-12-29 09:14:54 -0800

    Got tox passing with the load function now inside the JublerDataProcessor class.

--- src/packaged_logic_for_CI_CD/main.py
+++ src/packaged_logic_for_CI_CD/main.py
@@ -17,66 +17,72 @@ def func2(num: float) -> float:
     return num ** 2
 
 
-def load(text_file: str) -> pd.DataFrame:
-    """
-    Loads in Jubler .txt file and creates a pandas dataframe from it.
-    Args:
-        text_file: The .txt file path to be transformed into
-        pandas data frame.
-    Returns:
-        df: the created dataframe with start, stop, and label as columns.
-    """
+class JublerDataProcessor:
+    def __init__(self, num_processes=2):
+        self.num_processes = num_processes  # Defined for multi processing.
+        pass
 
-    # Regex used to extract the start and stop times from string in .txt file.
-    time_regex = r"(\b[0-9]+\b)"
+    @staticmethod
+    def load(text_file: str) -> pd.DataFrame:
+        """
+        Loads in Jubler .txt file and creates a pandas dataframe from it.
+        Args:
+            text_file: The .txt file path to be transformed into
+            pandas data frame.
+        Returns:
+            df: the created dataframe with start, stop, and label as columns.
+        """
 
-    # Regex used to extract the label from string in .txt file.
-    label_regex = r"[a-zA-Z]+"
-    if not os.path.exists(text_file):
-        return pd.DataFrame()
+        # Regex used to extract the start and stop times from string in .txt file.
+        time_regex = r"(\b[0-9]+\b)"
 
-    # Load the .txt data into pandas dataframe.
-    df = pd.read_csv(text_file, header=None)
+        # Regex used to extract the label from string in .txt file.
+        label_regex = r"[a-zA-Z]+"
+        if not os.path.exists(text_file):
+            return pd.DataFrame()
 
-    # Define simple data cleaning functions that are applied to jubler df.
-    def extract_time(input_str: str) -> Tuple[Any, ...]:
-        return tuple(re.findall(time_regex, input_str))
+        # Load the .txt data into pandas dataframe.
+        df = pd.read_csv(text_file, header=None)
 
-    def convert_string_to_int(input_str: str) -> float:
-        return int(input_str)
+        # Define simple data cleaning functions that are applied to jubler df.
+        def extract_time(input_str: str) -> Tuple[Any, ...]:
+            return tuple(re.findall(time_regex, input_str))
 
-    def extract_label(input_str: str) -> List[Any]:
-        return re.findall(label_regex, input_str)
+        def convert_string_to_int(input_str: str) -> float:
+            return int(input_str)
 
-    def clean_labels(input_str: str) -> str:
-        return input_str[0] if len(input_str) else "skip"
+        def extract_label(input_str: str) -> List[Any]:
+            return re.findall(label_regex, input_str)
 
-    # Extracting time using regex.
-    df["start"], df["stop"] = zip(*df[0].apply(extract_time))
+        def clean_labels(input_str: str) -> str:
+            return input_str[0] if len(input_str) else "skip"
 
-    # Converting start string to int and dividing by 10 to convert MPL2 to seconds.
-    df["start"] = df["start"].apply(convert_string_to_int) / 10
+        # Extracting time using regex.
+        df["start"], df["stop"] = zip(*df[0].apply(extract_time))
 
-    # Converting stop string to int and dividing by 10 to convert MPL2 to seconds.
-    df["stop"] = df["stop"].apply(convert_string_to_int) / 10
+        # Converting start string to int and dividing by 10 to convert MPL2 to seconds.
+        df["start"] = df["start"].apply(convert_string_to_int) / 10
 
-    # Extracting labels using regex.
-    df["label"] = df[0].apply(extract_label)
+        # Converting stop string to int and dividing by 10 to convert MPL2 to seconds.
+        df["stop"] = df["stop"].apply(convert_string_to_int) / 10
 
-    # Replacing empty instances with "skip" label.
-    df["label"] = df["label"].apply(clean_labels)
+        # Extracting labels using regex.
+        df["label"] = df[0].apply(extract_label)
 
-    # Drop unneeded column.
-    df = df.drop([0], axis=1)
+        # Replacing empty instances with "skip" label.
+        df["label"] = df["label"].apply(clean_labels)
 
-    # Return jubler df.
-    return df
+        # Drop unneeded column.
+        df = df.drop([0], axis=1)
+
+        # Return jubler df.
+        return df
 
 
 def main():
-    test_df = load(TEST_TEXT_FILE)
-    print(test_df.shape)
-    print(type(test_df))
+    # test_df = JublerDataProcessor().load(TEST_TEXT_FILE)
+    # print(test_df.shape)
+    # print(type(test_df))
     pass
 
 
--- tests/test_CI_CD.py
+++ tests/test_CI_CD.py
@@ -1,8 +1,8 @@
-from packaged_logic_for_CI_CD.main import func1, func2, load, TEST_TEXT_FILE
+from packaged_logic_for_CI_CD.main import func1, func2, JublerDataProcessor, TEST_TEXT_FILE
 
 import unittest  # Native module used for testing.
 
-print(load(TEST_TEXT_FILE).shape)
+print(JublerDataProcessor().load(TEST_TEXT_FILE).shape)
 
 
 class TestMyFunction(unittest.TestCase):
@@ -17,7 +17,8 @@ class TestMyFunction(unittest.TestCase):
     def test_load_1(self):
         """Tests if correct data frame is created by checking shape, and if the file exists."""
         # Testing for the correct shape.
-        test_df = load(TEST_TEXT_FILE)
+        pipeline = JublerDataProcessor()
+        test_df = pipeline.load(TEST_TEXT_FILE)
         self.assertIsNotNone(test_df)
         self.assertEqual(test_df.shape, (101, 3))
 
@@ -28,9 +29,9 @@ class TestMyFunction(unittest.TestCase):
 
 
 def main():
-    test_df = load(TEST_TEXT_FILE)
-    print(test_df.shape)
-    print(type(test_df))
+    # test_df = JublerDataProcessor().load(TEST_TEXT_FILE)
+    # print(test_df.shape)
+    # print(type(test_df))
     pass
 
 
